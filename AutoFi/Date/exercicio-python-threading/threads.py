d8a393e0de8eb69b066c65a64ba179338cdb3c56
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-07-06 23:30:07 +0800

    Update threads.py

--- AutoFi/Date/exercicio-python-threading/threads.py
+++ AutoFi/Date/exercicio-python-threading/threads.py
@@ -4,10 +4,10 @@ def msg(msg='None'):
     print(msg)
 
 thread = [
-        threading.Thread(msg('1025'), list['thread 1']),
-        threading.Thread(msg('1028'), list['thread 2']),
-        threading.Thread(msg('1111'), list['thread 3']),
-        threading.Thread(msg('1117'), list['thread 4']),
+        threading.Thread(msg('1025'), list['thread']),
+        threading.Thread(msg('1028'), list['thread']),
+        threading.Thread(msg('1111'), list['thread']),
+        threading.Thread(msg('1117'), list['thread']),
         ]
 
 
